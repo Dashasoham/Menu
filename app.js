4afcd5af8732d6672e11829dd4698aca391683e2
Dashasoham <dikrain8@gmail.com>
2022-12-27 22:35:47 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,122 @@
+const menu = [
+  {
+    id: 1,
+    title: 'poke bowl',
+    category: 'breakfast',
+    price: 10.9,
+    img: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2Njc4ODA&ixlib=rb-4.0.3&q=80',
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: 'egg toast mini',
+    category: 'breakfast',
+    price: 5.0,
+    img: 'https://images.unsplash.com/photo-1593584785033-9c7604d0863f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: 'keto lunch',
+    category: 'lunch',
+    price: 6.99,
+    img: 'https://images.unsplash.com/photo-1539136788836-5699e78bfc75?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: 'waffle delight',
+    category: 'sweet-breakfast',
+    price: 20.99,
+    img: 'https://images.unsplash.com/photo-1594627882045-57465104050f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: 'sandwich attack',
+    category: 'breakfast',
+    price: 5.9,
+    img: 'https://images.unsplash.com/photo-1475090169767-40ed8d18f67d?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: 'crepes a la berries',
+    category: 'sweet-breakfast',
+    price: 8.9,
+    img: 'https://images.unsplash.com/photo-1532980400857-e8d9d275d858?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: 'salmon poke bowl',
+    category: 'lunch',
+    price: 8.9,
+    img: 'https://images.unsplash.com/photo-1555243896-c709bfa0b564?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: 'tuna a la luna',
+    category: 'lunch',
+    price: 12.5,
+    img: 'https://images.unsplash.com/photo-1568600891621-50f697b9a1c7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: 'wok noodles',
+    category: 'lunch',
+    price: 16.99,
+    img: 'https://images.unsplash.com/photo-1526318896980-cf78c088247c?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzE2NjgxMjg&ixlib=rb-4.0.3&q=80',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItems(menu);
+});
+
+filterBtns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    if (category === 'all') {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+//filter buttons
